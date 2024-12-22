42c68a8ccefcc098bc12ce69927ad756bb963f15
Md. Ashraful Alam Sifat <56783196+sifat2626@users.noreply.github.com>
2024-12-22 15:31:18 +0600

    dist deleted

--- dist/app/modules/User/user.constant.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserRoleEnum = exports.USER_ROLE = void 0;
-exports.USER_ROLE = {
-    superAdmin: 'superAdmin',
-    admin: 'admin',
-    user: 'user',
-};
-exports.UserRoleEnum = ['superAdmin', 'admin', 'user'];
