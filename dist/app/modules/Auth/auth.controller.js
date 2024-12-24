0b4808b5c1defb58485362a6f6ae2cf261ceb4c4
Md. Ashraful Alam Sifat <56783196+sifat2626@users.noreply.github.com>
2024-12-24 18:01:24 +0600

    Update auth.controller.js

--- dist/app/modules/Auth/auth.controller.js
+++ dist/app/modules/Auth/auth.controller.js
@@ -14,7 +14,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AuthControllers = void 0;
 const http_status_1 = __importDefault(require("http-status"));
-const config_1 = __importDefault(require("../../config"));
 const catchAsync_1 = __importDefault(require("../../utils/catchAsync"));
 const sendResponse_1 = __importDefault(require("../../utils/sendResponse"));
 const auth_service_1 = require("./auth.service");
@@ -22,7 +21,7 @@ const loginUser = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void
     const result = yield auth_service_1.AuthServices.loginUser(req.body);
     const { refreshToken, accessToken } = result;
     res.cookie('refreshToken', refreshToken, {
-        secure: config_1.default.NODE_ENV === 'production',
+        secure: true,
         httpOnly: true,
         sameSite: 'none',
         maxAge: 1000 * 60 * 60 * 24 * 365,
